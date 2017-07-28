4b058d167e412e644c822c73e6db17311927e427
smallX <xieyu559@163.com>
2017-07-29 02:22:58 +0800

    add files

--- Label_attrabuted/ViewController.m
+++ Label_attrabuted/ViewController.m
@@ -16,7 +16,22 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(50, 100, 200, 120)];
+    label.backgroundColor = [UIColor lightGrayColor];
+    NSString *text = @"123456789011";
+    NSMutableAttributedString *string = [self setTextColor:[UIColor redColor] range:NSMakeRange(0, text.length - 5) text:text];
+    label.attributedText = string;
+    [self.view addSubview:label];
+}
+
+- (NSMutableAttributedString *)setTextColor:(UIColor *)textColor range:(NSRange)range text:(NSString *)text
+{
+    NSAttributedString *string = [[NSAttributedString alloc] initWithString:text];
+    NSMutableAttributedString *str = [[NSMutableAttributedString alloc] initWithAttributedString:string];
+    [str addAttribute:NSForegroundColorAttributeName
+                value:textColor
+                range:range];
+    return str;
 }
 
 - (void)didReceiveMemoryWarning {
